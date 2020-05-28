b1be2289e2b5d85bbf9620c7fb9c5e577b5f5863
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2020-05-28 16:23:22 -0400

    Update computerplayer.cpp

--- computerplayer.cpp
+++ computerplayer.cpp
@@ -1,9 +1,4 @@
-// CISC 2000  Fordham University  Spring 2020
-// Instructor: Vincent Mierlak
-// 
-// Name: Md Fazle Rabbi
-// 
-// HW 9: The Game of Pig.
+// The Game of Pig.
 // File: computerplayer.cpp
 //       Implementation of ComputerPlayer, a dervied class for the 
 //       computer to play the game of Pig.
